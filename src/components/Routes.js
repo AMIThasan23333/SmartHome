4b1b7f2807d855a72332eedfe81d20cebe1f37d8
My Name <cpgsom819@gmail.com>
2023-04-25 08:40:23 +0800

    adding route

---
+++ src/components/Routes.js
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from './Home';
+
+const router = createBrowserRouter([
+
+    {
+
+        path : '/',
+        element : <Home></Home>
+  
+  
+    }
+    
+  ])
+
+
+  export default router;
